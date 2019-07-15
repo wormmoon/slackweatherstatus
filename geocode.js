4e97feb78dff1f83f6f61778d4d528889ab70b99
Emily Oliver <mleeoliver@gmail.com>
2019-07-15 14:57:54 +0100

    Update geocode.js

--- geocode.js
+++ geocode.js
@@ -2,19 +2,22 @@ require('dotenv').config();
 const getWeather = require('./getWeather');
 const request = require('request');
 
-request('https://www.mapquestapi.com/geocoding/v1/address?key=ZQznL56SwOubzsr6rwsWArp1Mu0vF8A0&location=Brighton', { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
-  console.log('geocode:', body.results[0].locations[0].latLng);
-  const latLng = body.results[0].locations[0].latLng;
+const geocode = function(place) {
 
-  getWeather(latLng);
-});
+  return new Promise(function(resolve, reject) {
+    request(`https://www.mapquestapi.com/geocoding/v1/address?key=ZQznL56SwOubzsr6rwsWArp1Mu0vF8A0&location=${place}`, { json: true }, (err, res, body) => {
+      if (err) {
+        // return console.log(err);
+        reject(Error(err));
+      } else {
+        // console.log('geocode:', body.results[0].locations[0].latLng);
+        const latLng = body.results[0].locations[0].latLng;
 
-// console.log(body);
-// console.log(lng);
+        // getWeather(latLng);
+        resolve(latLng);
+      }
+    });
+  })
+}
 
-// Get location from command and geocode in to long and lat
-// const geoData = `http://www.mapquestapi.com/geocoding/v1/address?key=ZQznL56SwOubzsr6rwsWArp1Mu0vF8A0&location=Brighton`;
-
-// module.exports = lat, lng;
-
+module.exports = geocode;
